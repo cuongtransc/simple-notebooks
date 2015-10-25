3f5f981e7c3f16146f2f5d70ff919f564e8ac0c9
Tran Huu Cuong <tranhuucuong91@gmail.com>
2015-10-25 23:09:37 +0700

    Fix comment typo

--- db_create.py
+++ db_create.py
@@ -5,6 +5,6 @@ import os
 from config import PATH_SQLITE_DB
 from app import database, Entry, FTSEntry
 
-# init database if don't exits
+# init database if don't exist
 if not os.path.exists(PATH_SQLITE_DB):
     database.create_tables([Entry, FTSEntry], safe=True)
