6d83381841da3c06783ad17250ad7af2a61af871
Tran Huu Cuong <tranhuucuong91@gmail.com>
2015-11-04 13:24:59 +0700

    Update db_create: fix missing database model

--- db_create.py
+++ db_create.py
@@ -3,8 +3,9 @@
 
 import os
 from config import PATH_SQLITE_DB
-from app import database, Entry, FTSEntry
+from app import database
+from app.models.entry import Entry
 
 # init database if don't exist
 if not os.path.exists(PATH_SQLITE_DB):
-    database.create_tables([Entry, FTSEntry], safe=True)
+    database.create_tables([Entry], safe=True)
